eb7380d22e94d80e531900fbd068eadfce20c721
Jeneen <jeneen.lemar@gmail.com>
2020-02-20 20:21:28 -0500

    adds index and new actions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,21 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+
+    <h1>NEW Place</h1>
+    <div>
+      Name:
+      <input type="text" v-model="newPlaceName">
+      Address:
+      <input type="text" v-model="newPlaceAddress">
+      <button v-on:click="createPlace()">Create Place</button>
+    </div>
+
+
+    <div v-for="place in places">
+      <h2>{{place.name}}</h2>
+      
+    </div>
   </div>
 </template>
 
@@ -10,13 +25,36 @@
 </style>
 
 <script>
+import axios from "axios";
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Places App!",
+      places: [],
+      newPlaceAddress: "",
+      newPlaceName: ""
     };
   },
-  created: function() {},
-  methods: {}
+  created: function() {
+    axios.get("/api/places").then(response => {
+      this.places = response.data;
+    });
+  },
+  methods: {
+    createPlace: function() {
+      var params = {
+        name: this.newPlaceName,
+        address: this.newPlaceAddress
+      };
+      axios.post("api/places", params).then(response => {
+        this.places.push(response.data);
+        this.newPlaceName = "";
+        this.newPlaceAddress = "";
+
+
+      });
+
+    }
+  }
 };
 </script>
